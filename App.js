a5f4d710375f981e5af27691cd149314fd9f680d
BoyuL129 <boyuliu9@ad.unc.edu>
2023-12-05 20:06:07 +0000

    1

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+var registerRouter = require('./routes/register');
+
 
 var app = express();
 
@@ -23,16 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
-
-app.get('/search', async (req, res, next) => {
-  try {
-      const { term, sortBy, order } = req.query;
-      const results = await db.search(term, sortBy, order);
-      res.render('result', { results, searchTerm: term });
-  } catch (error) {
-      next(error);
-  }
-});
+app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
