16cac44c20062ef1813732564058558f340ad820
ruiying9 <92762753+ruiying9@users.noreply.github.com>
2023-12-05 15:18:15 -0600

    Update video.js

---
+++ controllers/video.js
@@ -0,0 +1,35 @@
+// controllers/video.js
+const db = require('../db.js');
+
+exports.searchVideos = function(req, res) {
+  const term = req.query.term;
+  const sortBy = req.query.sortBy;
+  const order = req.query.order;
+  
+  db.search(term, sortBy, order, function(results) {
+    res.render('videos', { videos: results });
+  });
+};
+s
+exports.saveVideo = function(req, res) {
+  const userId = req.user.id; // Assuming you have authentication and the user's ID is available
+  const videoId = req.body.videoId; // The ID of the video to save, sent from the frontend
+
+  db.saveVideoForUser(userId, videoId, function(results) {
+    res.json({ success: true, message: "Video saved successfully." });
+  });
+};
+
+
+// routes/videos.js
+const express = require('express');
+const router = express.Router();
+const videoController = require('../controllers/video');
+
+// ... other video routes ...
+
+// Route to handle saving a video
+router.post('/save', videoController.saveVideo);
+
+module.exports = router;
+
