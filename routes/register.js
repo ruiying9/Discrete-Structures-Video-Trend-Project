ae2446046804ee9ce163f1573197a344b899878f
BoyuL129 <boyuliu9@ad.unc.edu>
2023-12-05 20:05:08 +0000

    register

--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/userController');
 
 /* GET register page. */
 router.get('/', function(req, res, next) {
   res.render('register');
 });
 
+// Route for handling user registration
+router.post('/', userController.addUser);
 
 module.exports = router;
