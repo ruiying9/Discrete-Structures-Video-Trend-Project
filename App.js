a406ffd9a82a51583dbd55dc87ddfdbdfd466cea
ruiying9 <92762753+ruiying9@users.noreply.github.com>
2023-12-05 15:17:24 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,46 +1,31 @@
+// app.js
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// ... other imports ...
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
-var registerRouter = require('./routes/register');
-
+var videosRouter = require('./routes/videos'); // Include this only if videos.js exists in routes
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+// view engine setup and middleware...
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/login', loginRouter);
-app.use('/register', registerRouter);
+app.use('/videos', videosRouter); // Include this only if videos.js exists in routes
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// catch 404 and forward to error handler...
+// error handler...
 
 module.exports = app;
+
+// app.js
+// ... other imports and setup ...
+
+var videosRouter = require('./routes/videos');
+
+// ... other app.use middleware ...
+
+app.use('/videos', videosRouter);
+
+// ... rest of your app.js code ...
