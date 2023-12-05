0980bd0b7d7ae99668c2bb0efd4d86003d8268f1
ruiying9 <92762753+ruiying9@users.noreply.github.com>
2023-12-05 15:17:43 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,28 +1,36 @@
+// db.js
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host     : '34.173.221.22',
-  user     : 'root',
-  password : 'test1234',
-  database : 'mydatabase'
+  host: '34.173.221.22',
+  user: 'root',
+  password: 'test1234',
+  database: 'mydatabase'
 });
-
-const search = async (term, sortBy = 'Video_ID', order = 'ASC') => {
-  const query = "SELECT * FROM Video WHERE Video.Title LIKE ? ORDER BY ?? " + order;
-  const results = await connection.query(query, [`%${term}%`, sortBy]);
-  return results;
-}
-
 
 connection.connect((err) => {
   if (err) throw err;
+  console.log('Database connection established successfully.');
 });
 
-connection.on('connect', () => {
-    console.log('Database connection established successfully.');
+const search = (term, sortBy = 'Video_ID', order = 'ASC', callback) => {
+  const query = `SELECT * FROM Video WHERE Title LIKE ? ORDER BY ?? ${order}`;
+  connection.query(query, [`%${term}%`, sortBy], function(error, results, fields) {
+    if (error) throw error;
+    callback(results);
   });
+};
+
+const saveVideoForUser = (userId, videoId, callback) => {
+  const query = "INSERT INTO SavedVideos (userId, videoId) VALUES (?, ?)";
+  connection.query(query, [userId, videoId], function(error, results, fields) {
+    if (error) throw error;
+    callback(results);
+  });
+};
 
 module.exports = {
   connection,
-  search
+  search,
+   saveVideoForUser
 };
