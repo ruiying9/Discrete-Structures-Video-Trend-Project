0299d35dad4d5eb19f924064214995576094c906
ruiying9 <92762753+ruiying9@users.noreply.github.com>
2023-12-05 15:37:15 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-// app.js
 var createError = require('http-errors');
 var express = require('express');
-// ... other imports ...
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+// Require your route handlers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var registerRouter = require('./routes/register');
 var videosRouter = require('./routes/videosRouter');
 
 var app = express();
 
-// view engine setup and middleware...
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Middleware setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use your route handlers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/videos', videosRouter); // Include this only if videos.js exists in routes
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
+app.use('/videos', videosRouter); // Use the videos router for video-related routes
 
-// catch 404 and forward to error handler...
-// error handler...
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+  // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
