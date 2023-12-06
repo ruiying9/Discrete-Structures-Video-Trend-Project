b6a60d48889ccbe2a708f90a3778e47dd373841c
BoyuL129 <boyuliu9@ad.unc.edu>
2023-12-06 21:17:27 +0000

    1

--- routes/results.js
+++ routes/results.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const router = express.Router()
-const userController = require('../controllers/userController');
+const express = require('express');
+const router = express.Router();
+const videoController = require('../controllers/video');
+
 
 /* GET result page. */
 router.get('/', function(req, res, next) {
--- routes/videos.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const videoController = require('../controllers/video');
-
-router.get('/search', videoController.searchVideos);
-module.exports = router;
