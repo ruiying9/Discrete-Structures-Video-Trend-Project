ad27f95cec358d75bbfd8eb58744ce36f63759ae
BoyuL129 <boyuliu9@ad.unc.edu>
2023-12-06 05:57:54 +0000

    1

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var registerRouter = require('./routes/register');
-var videosRouter = require('./routes/videos');
+//var videosRouter = require('./routes/videos');
 
 var app = express();
 
@@ -29,7 +29,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
-app.use('/videos', videosRouter); // Use the videos router for video-related routes
+//app.use('/videos', videosRouter); // Use the videos router for video-related routes
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
