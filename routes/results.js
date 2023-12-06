472350319b62a6a78d582f97875bff24d82d9991
ruiying9 <92762753+ruiying9@users.noreply.github.com>
2023-12-06 17:21:26 -0600

    Update results.js

--- routes/results.js
+++ routes/results.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const videoController = require('../controllers/video');
 
-
+router.post('/delete', videoController.deleteVideo);
 /* GET result page. */
 router.get('/', function(req, res, next) {
     res.render('results');
