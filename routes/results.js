3fcad9b9202fb387c83a87fe4f2190878ecf47b2
BoyuL129 <boyuliu9@ad.unc.edu>
2023-12-07 01:07:51 +0000

    1

--- routes/results.js
+++ routes/results.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const videoController = require('../controllers/video');
 
-router.get('/', function(req, res, next) {
-  res.render('results');
-});
+router.get('/results', (req, res) => {
+  const term = req.query.term;
 
+  videoController.searchVideos(term, sortBy, order, (results) => {
+      res.render('results', { videos: results });
+  });
+});
 
 //router.post('/:id', videoController.saveVideo);
 
